26e249d85dc3625c762eedf7647865435a590b67
DuaneQ <DuaneQHodges@gmail.com>
2025-04-17 20:27:22 +0000

    Style OctoFit Tracker: Updated App.css, added logo, favicon, and Bootstrap styling for all components

---
+++ octofit-tracker/frontend/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Activities from './components/Activities';
+import Leaderboard from './components/Leaderboard';
+import Teams from './components/Teams';
+import Users from './components/Users';
+import Workouts from './components/Workouts';
+import './App
